22f3fcb002fbe068735337ef889fdf54cfd310bb
sirsona <swaleh489@gmail.com>
2026-07-30 16:26:21 +0300

    feat: add robots.js

---
+++ app/robots.js
@@ -0,0 +1,10 @@
+// app/robots.js
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "http://localhost:3000/sitemap.xml",
+  };
+}
